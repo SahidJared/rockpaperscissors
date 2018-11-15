ca95b6cf6d2b330140d2834b05800240bf215197
Sahid Gutierrez <s.gutierrez@ihsph.org>
2018-11-15 23:10:59 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -10,6 +10,11 @@
 // DOCUMENT READY FUNCTION BELOWs
 $(document).ready(function(){
     $("#shoot").click(function(){
-    $("#userChoice").text($("shoot").val());
+        $("#userChoice").text($("#shoot").val());
+        alert(randomNumber);
     });
 });
+var computerChoice="";
+var winner="";
+var userChoice="";
+var randomNumber=Math.random();
